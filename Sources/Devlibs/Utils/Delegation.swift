903d2d50c7258041c0df3cc03d0e9349956bb3b6
Charles Hsieh <charlous167@gmail.com>
2021-01-14 13:53:13 +0800

    Add Delegation.swift in Utils

---
+++ Sources/Devlibs/Utils/Delegation.swift
@@ -0,0 +1,47 @@
+/// A wrapper of a closure that keeps a weak reference to the target object,
+/// avoiding retain-cycles caused by strongly referencing the object in the closure.
+public final class Delegation<Input, Output> {
+    private var closure: ((Input) -> Output?)?
+
+    private init() {}
+
+    public static func create<Target: AnyObject>(
+        on target: Target,
+        closure: @escaping (Target, Input) -> Output
+    ) -> Delegation<Input, Output> {
+        let delegation = Delegation<Input, Output>()
+        delegation.delegate(on: target, closure: closure)
+        return delegation
+    }
+
+    @discardableResult
+    public func invoke(_ input: Input) -> Output? {
+        return closure?(input)
+    }
+
+    @discardableResult
+    public func callAsFunction(_ input: Input) -> Output? {
+        return invoke(input)
+    }
+
+    private func delegate<T: AnyObject>(on target: T, closure: @escaping (T, Input) -> Output) {
+        self.closure = { [weak target] input in
+            guard let target = target else {
+                return nil
+            }
+            return closure(target, input)
+        }
+    }
+}
+
+extension Delegation where Input == Void {
+    @discardableResult
+    public func invoke() -> Output? {
+        return invoke(())
+    }
+
+    @discardableResult
+    public func callAsFunction() -> Output? {
+        return invoke()
+    }
+}
